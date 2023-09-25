b4889f5272194cffeb10ad6185e348bd93b3fe8e
Conor <conor.reed@gmail.com>
2023-09-25 17:40:04 -0500

    added some global variables for the javascript and built the startTimer function

--- assets/script.js
+++ assets/script.js
@@ -12,21 +12,90 @@
 
 
 //create any necessary global variables
+const questionOne = {A:"",B:"",C:"",D:""};
+const questionTwo = {A:"",B:"",C:"",D:""};
+const questionThree = {A:"",B:"",C:"",D:""};
+const questionFour = {A:"",B:"",C:"",D:""};
+const questionFive = {A:"",B:"",C:"",D:""};
+const questionSix = {A:"",B:"",C:"",D:""};
+const questionSeven = {A:"",B:"",C:"",D:""};
+const questionEight = {A:"",B:"",C:"",D:""};
+const questionNine = {A:"",B:"",C:"",D:""};
+const questionTen = {A:"",B:"",C:"",D:""};
+
+
+// Function to stop the timer
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+// Function to end the quiz
+function endQuiz() {
+  // Display the score screen or any other actions you need to take when the quiz ends
+}
+
+// Example usage: Call startTimer() when you start the quiz
+startTimer();
+const quizDurationSeconds = 60;  
+let timeRemaining = quizDurationSeconds;
+let timerInterval;
+
+// Function to start the timer
+function startTimer() {
+  // Update the timer every second
+  timerInterval = setInterval(function() {
+    // Display the time remaining
+    document.getElementById('timer').textContent = `Time: ${timeRemaining}s`;
+
+    // Check if the time has run out
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    } else {
+      // Decrease the time by 1 second
+      timeRemaining--;
+    }
+  }, 1000);
+}
+
+// Function to stop the timer
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+// Function to end the quiz
+function endQuiz() {
+  // Display the score screen or any other actions you need to take when the quiz ends
+}
+
+// Example usage: Call startTimer() when you start the quiz
+startTimer();
+
 
 
 //implement functionality for starting the timer
 
-
 // present the questions
+function showQuestion(){
 
+}
 
 //handling user answers
+function checkAnswers(){
 
+}
 
 //subtracting time for incorrect answers
+function subtractTime(){
 
+}
 
 //ending the game
+function endGame(){
 
+}
 
 // allow user to save their initials and score
+function saveScore(){
+
+}
